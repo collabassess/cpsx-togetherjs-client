92c41f5040baac441b594c7a8eb9f4dbca4f5945
Ajay Thorve <ajaythorve1994@gmail.com>
2017-12-21 20:48:16 -0500

    Udpate2

--- build/togetherjs/ui.js
+++ build/togetherjs/ui.js
@@ -174,14 +174,14 @@ define(["require", "jquery", "util", "session", "templates", "templating", "link
 
 
 
-    TogetherJS.config.track("disableWebRTC", function (hide) {
-      if (hide) {
-        ui.container.find("#togetherjs-audio-button").show();
-        adjustDockSize(1);
-      } else {
-        ui.container.find("#togetherjs-audio-button").hide();
-        adjustDockSize(-1);
-      }
+    TogetherJS.config.track("disableWebRTC", function (hide, previous) {
+        if (hide && ! previous) {
+            ui.container.find("#togetherjs-audio-button").hide();
+            adjustDockSize(-1);
+        } else if ((! hide) && previous) {
+            ui.container.find("#togetherjs-audio-button").show();
+            adjustDockSize(1);
+        }
     });
 
   };
